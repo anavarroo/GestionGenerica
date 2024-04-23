076b1bcf01829746c92ed13734d35adbff818801
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-04-23 15:49:57 +0200

    Metetodo buscar por el correo y base del metodo modificar usuario

--- src/main/java/com/api/gestiongenerica/services/UserServiceI.java
+++ src/main/java/com/api/gestiongenerica/services/UserServiceI.java
@@ -1,8 +1,17 @@
 package com.api.gestiongenerica.services;
 
 
+import com.api.gestiongenerica.persistence.dto.UserDto;
+
 public interface UserServiceI {
 
-
+    /**
+     * Actualiza la descripción de un usuario.
+     *
+     * @param correo Correo del usuario a actualizar.
+     * @param userDto DTO del usuario con la nueva información.
+     * @return DTO del usuario actualizado.
+     */
+    UserDto actualizarUsuario(String correo, UserDto userDto);
 
 }
--- src/main/java/com/api/gestiongenerica/services/UserServiceImp.java
+++ src/main/java/com/api/gestiongenerica/services/UserServiceImp.java
@@ -1,5 +1,7 @@
 package com.api.gestiongenerica.services;
 
+import com.api.gestiongenerica.persistence.dto.UserDto;
+import com.api.gestiongenerica.persistence.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,17 @@ import com.api.gestiongenerica.persistence.repository.UserRepositoryI;
 @Service
 public class UserServiceImp implements UserServiceI{
 
+    private final UserRepositoryI userRepositoryI;
 
     @Autowired
-    private UserRepositoryI userRepositoryI;
-    
+    public UserServiceImp(UserRepositoryI userRepositoryI) {
+        this.userRepositoryI = userRepositoryI;
+    }
+
+    @Override
+    public UserDto actualizarUsuario(String correo, UserDto userDto) {
+        User user = userRepositoryI.findByCorreo(correo);
+        user.setNombre(userDto.getNombre());
+        return userDto;
+    }
 }
